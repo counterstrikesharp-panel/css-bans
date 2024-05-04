83c12c7f4151b0040fc52d7bc78564bae9ae8378
hobsRKM <yuvim86@gmail.com>
2024-05-04 21:03:45 +0530

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -17,6 +17,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->withoutMiddleware()->get('/');
 
-        $response->assertRedirect();
+        $response->assertStatus(200);
     }
 }
