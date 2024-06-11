00b428fc5d11329e4de0c0d96114ded260c9b928
AiDN™ <45371311+originalaidn@users.noreply.github.com>
2024-06-11 18:06:47 +0200

    added Ukrainian language

--- resources/views/components/navbar/style-vertical-menu.blade.php
+++ resources/views/components/navbar/style-vertical-menu.blade.php
@@ -37,6 +37,7 @@
                         <option {{ app()->getLocale() == 'zh_CN' ? 'selected' : '' }} value="zh_CN" data-image="{{ Vite::asset('resources/images/1x1/cn.svg') }}">Chinese</option>
                         <option {{ app()->getLocale() == 'ro' ? 'selected' : '' }} value="ro" data-image="{{ Vite::asset('resources/images/1x1/ro.svg') }}">Romanian</option>
                         <option {{ app()->getLocale() == 'cz' ? 'selected' : '' }} value="cs" data-image="{{ Vite::asset('resources/images/1x1/cz.svg') }}">Czech</option>
+                        <option {{ app()->getLocale() == 'uk' ? 'selected' : '' }} value="uk" data-image="{{ Vite::asset('resources/images/1x1/uk.svg') }}">Ukrainian</option>
 
                     </select>
                 </li>
