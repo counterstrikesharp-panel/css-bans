644a8712a04dda17f463129be1dfc5d6c0d1e6b6
hobsRKM <yuvim86@gmail.com>
2024-06-12 11:41:59 +0000

    testcases fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,18 +3,19 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
-     *
-     * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->withoutMiddleware()->get('/requirement');
 
         $response->assertStatus(200);
     }
